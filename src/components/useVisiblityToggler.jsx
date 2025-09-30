5d1ce171a2d90e023161883ee373e65786340a81
Raj Mehra <96688964+Rajmehransut@users.noreply.github.com>
2025-10-01 01:31:44 +0530

    Delete src directory

--- src/components/useVisiblityToggler.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const useVisiblityToggle = (component, visiblity = false) => {
-  const [visible, setVisiblity] = useState(() => visiblity);
-  return [visible ? component : null, () => setVisiblity((v) => !v)];
-};
-
-export default useVisiblityToggle;
